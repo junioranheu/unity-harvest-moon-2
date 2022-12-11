9db6cb25ce0b34fc85c6a9a95e74c207543cc367
junioranheu <junioranheu@gmail.com>
2022-12-11 00:41:58 -0300

    tut 2 - camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,8 @@ public class CameraController : MonoBehaviour
 {
     Transform playerPos;
     private readonly float offsetZ = 5f;
- 
+    private readonly float smoothing = 1f;
+
     void Start()
     {
         // Encontrar o game object do personagem na cena e tranformá-lo em um componente;
@@ -19,6 +20,6 @@ public class CameraController : MonoBehaviour
     private void FollowPlayer()
     {
         Vector3 targetPosition = new(playerPos.position.x, transform.position.y, (playerPos.position.z - offsetZ));
-        transform.position = targetPosition;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, (smoothing * Time.deltaTime));
     }
 }
